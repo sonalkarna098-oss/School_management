0146ff57a98846ce2a154687ae54bb098eb12b72
JR Sonal <your_email@gmail.com>
2026-03-13 18:20:28 +0530

    first commit

--- app.py
+++ app.py
@@ -438,4 +438,5 @@ def apply():
     return redirect(url_for('careers'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
