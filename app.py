953637921f13b0c26ee0e7d2577d7fe6a2216d11
JR Sonal <your_email@gmail.com>
2026-03-19 14:35:27 +0530

    final12 committdft

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ app.secret_key = os.getenv('SECRET_KEY', 'super_secret_school_key')
 
 # Email Setup
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
